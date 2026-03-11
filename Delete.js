5c47926bb5f20622d51474b79cf9267631c02643
mahesh sahu <shamhesh615@gmail.com>
2026-03-11 13:24:03 +0530

    Create Delete.js

---
+++ Delete.js
@@ -0,0 +1,9 @@
+let arr = [10,20,30,40,50,60,70,80,90,100];
+let position = 3;
+
+for (let i = position; i <= arr.length-1; i++) {
+    arr[i]=arr[i+1]
+    
+}
+arr.length = arr.length-1
+console.log(arr);
