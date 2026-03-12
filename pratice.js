c9d280d693e5dc61a636f79c8e7867bac86c3272
mahesh sahu <shamahesh615@gmail.com>
2026-03-12 20:34:27 +0530

    Create pratice.js

---
+++ pratice.js
@@ -0,0 +1,13 @@
+//array insert
+
+let arr = [10,20,30,40,50,60,70,80,90,100];
+let position = 3;
+let element = 35;
+
+
+for(let i = arr.length-1;i>=0;i--){
+    console.log(arr[i]);
+    
+    if
+    
+}
