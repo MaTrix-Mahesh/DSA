0437a37922582e61d4ec90823f919ba7cd16b9b9
mahesh sahu <shamahesh615@gmail.com>
2026-03-18 20:11:28 +0530

    Update pratice.js

--- pratice.js
+++ pratice.js
@@ -1,20 +1,14 @@
-//array insert
+let arr = [10,20,30,40,50];
+let position = 2;
+let newelement = 24;
 
-let arr = [10,20,30,40,50,60,70,80,90,100];
-let position = 3;
-let element = 35;
-
-
-for(let i = arr.length-1;i>=0;i--){
-    //console.log(arr[i]);
-    
-    if( i>= position){
+for (let i = arr.length-1; i >=0 ; i--) {
+    if (i>=position) {
         arr[i+1] = arr[i];
         if (i==position) {
-            arr[i]= element;
+            arr[i]= newelement;
         }
     }
-    
 }
+
 console.log(arr);
-console.log("hello world ");
