9d846da3cf1d7fb285eda1b5792776053858257c
mahesh sahu <shamahesh615@gmail.com>
2026-03-23 21:34:19 +0530

    Update mergeArray.js

--- mergeArray.js
+++ mergeArray.js
@@ -25,5 +25,20 @@ console.log(merge);
 
 */
 
-console.log("HKFHSJBJICBH");
-console.log("hjufbkjbkjb");
+
+
+let arr1 = [10,20,30,40,50,60];
+
+let arr2 = [5,6,7,8,9];
+
+let arr3 = []
+
+for (let i = 0; i < arr1.length; i++) {
+    arr3[i] = arr1[i]
+}
+
+for (let i = 0; i < arr2.length; i++) {
+    arr3[arr1.length+i ] = arr2[i];
+    
+}
+ console.log(arr3)
