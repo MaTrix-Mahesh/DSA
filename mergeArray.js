84aff8293e7d25e51025431f67e1269dd4ce7c35
mahesh sahu <shamahesh615@gmail.com>
2026-03-23 18:12:01 +0530

    initial commit

---
+++ mergeArray.js
@@ -0,0 +1,26 @@
+
+/**
+ * 
+ * USING SPREAD OPERATOR
+ 
+let arr1 = [10,20,30,40,50,60];
+let arr2 = [5,6,7,8,9];
+
+let merge = [...arr1,...arr2]
+console.log(merge);
+ */
+
+
+
+/**
+ * 
+// USING CONCAT FUNCTION
+
+let arr1 = [10,20,30,40,50,60];
+let arr2 = [5,6,7,8,9];
+
+let merge = arr1.concat(arr2)
+
+console.log(merge);
+
+*/
