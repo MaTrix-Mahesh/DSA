a660699858fa8e01039ec6900f52db686d6ea1c5
mahesh sahu <shamahesh615@gmail.com>
2026-03-23 21:50:39 +0530

    initial commit

---
+++ Reverse.js
@@ -0,0 +1,8 @@
+ //REVERSE OF ARRAY 
+
+ let arr = [10,20,30,40,50,60,70,80,90,100];
+
+ for (let i = arr.length-1; i >= 0; i--) {
+    console.log(arr[i]);
+    
+ }
--- mergeArray.js
+++ mergeArray.js
@@ -27,29 +27,19 @@ console.log(merge);
 
 
 //-----------------------------------------------------------------------------------------
-// let arr1 = [10,20,30,40,50,60];
+let arr1 = [10,20,30,40,50,60];
+let arr2 = [5,6,7,8,9];
+let arr3 = []
 
-// let arr2 = [5,6,7,8,9];
+for (let i = 0; i < arr1.length; i++) {
+    arr3[i] = arr1[i]
+}
 
-// let arr3 = []
-
-// for (let i = 0; i < arr1.length; i++) {
-//     arr3[i] = arr1[i]
-// }
-
-// for (let i = 0; i < arr2.length; i++) {
-//     arr3[arr1.length+i ] = arr2[i];
+for (let i = 0; i < arr2.length; i++) {
+    arr3[arr1.length+i ] = arr2[i];
     
-// }
-//  console.log(arr3)
+}
+ console.log(arr3)
 
 
 //---------------------------------------------------------------------------------------------
- //REVERSE OF ARRAY 
-
-let arr = [10,20,30,40,50,60,70,80,90,100];
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    
-    
-}
