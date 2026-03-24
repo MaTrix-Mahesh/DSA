1dff84e54c28da812a42adb95fa4b4edcbd5fa5b
mahesh sahu <shamahesh615@gmail.com>
2026-03-24 20:34:19 +0530

    initial commit

--- mergeArray.js
+++ mergeArray.js
@@ -43,12 +43,23 @@ console.log(merge);
 
 
 //---------------------------------------------------------------------------------------------
-
+//----------- using while loop 
 
 let arr1 = [10,20,30,40,50];
 let arr2 = [60,70,80,90,100];
 
 let arr3 = [];
+
+
+for (let i = 0; i < arr1.length; i++) {
+    arr3.push(arr1[i])
+}
+
+for (let i = 0; i < arr2.length; i++) {
+    arr3.push(arr2[i])
+}
+
+console.log(arr3);
 
 
 
