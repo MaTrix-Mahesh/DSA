76bd083ebb795769bcb7353def2d0527c742484b
mahesh sahu <shamahesh615@gmail.com>
2026-03-31 21:27:53 +0530

    initial commit

---
+++ Interview/pratice.js
@@ -0,0 +1,22 @@
+// let abc = "naused"
+// abc[2] ="r"
+// console.log(abc);
+ // naused
+ //---instead of removing square barket [2] , then what will be the output --//r
+
+
+
+//-------------------------------------------------------------------------------------------------
+
+//  let a  = 100;
+//  let z = a++; //  z = 100 , a=101 ------post-increment   
+//  console.log(a+z);//101+100
+ 
+
+//-------------------------------------------------------------------------------------------------
+
+let str = "my name is xyz";
+let newstr = str.split(" ").reverse()
+
+console.log(newstr);
+
