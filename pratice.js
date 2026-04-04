4480d58c8c5b71441bea62d18a49fa4b7d8f7e9c
mahesh sahu <shamahesh615@gmail.com>
2026-04-04 18:17:45 +0530

    intial commit

--- pratice.js
+++ pratice.js
@@ -1,14 +1,11 @@
 let arr = [10,20,30,40,50];
-let position = 2;
-let newelement = 24;
+let insertelement = 35;
+let position = 3
 
-for (let i = arr.length-1; i >=0 ; i--) {
-    if (i>=position) {
-        arr[i+1] = arr[i];
-        if (i==position) {
-            arr[i]= newelement;
-        }
-    }
+//console.log(arr.length);
+
+for (let i = arr.length-1; i>=0; i--) {
+   console.log(arr[i]);
+   
+    
 }
-
-console.log(arr);
