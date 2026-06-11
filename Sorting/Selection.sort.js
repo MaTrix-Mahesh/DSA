dc4a63e0ac6f12197d4c98209ca398c486773645
mahesh sahu <shamahesh615@gmail.com>
2026-06-11 08:39:51 +0530

    initial commit

---
+++ Sorting/Selection.sort.js
@@ -0,0 +1,23 @@
+let arr = [10,5,1,12,19]
+let n = arr.length
+
+let count =0
+
+for (let i = 0; i < n-1; i++) {
+    let minIndex = i
+    for (let j = i+1; j < n; j++) {
+        if (arr[minIndex] > arr[j]) {
+            minIndex = j
+        }
+        
+    }
+    count++;
+    let temp = arr[i]
+    arr[i] = arr[minIndex]
+    arr[minIndex] = temp
+    
+}
+
+console.log(arr);
+console.log(count);
+
