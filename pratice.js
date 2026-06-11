1127f3fbd3489a1c2661ec2417b5391e269f6272
mahesh sahu <shamahesh615@gmail.com>
2026-06-11 18:14:17 +0530

    initial commit

--- pratice.js
+++ pratice.js
@@ -1,11 +1,20 @@
-let arr = [10,20,30,40,50];
-let insertelement = 35;
-let position = 3
-
-//console.log(arr.length);
-
-for (let i = arr.length-1; i>=0; i--) {
-   console.log(arr[i]);
+let arr = [10,5,1,19,12];
+let n = arr.length
+let count = 0 
+for (let i = 0; i < n-1; i++) {
+   let minindex = i
+   for (let j = i+1; j <n; j++) {
+      count++;
+      if (arr[minindex] > arr[j]) {
+         minindex = j
+      }
+   }
+   let temp = arr[i]
+   arr[i] = arr[minindex]
+   arr[minindex] = temp 
    
-    
 }
+console.log(arr);
+console.log(count);
+
+
