b1de7f24832aa48086b9e9bd90a3a4886dc6b8b3
mahesh sahu <shamahesh615@gmail.com>
2026-07-24 21:37:35 +0530

    intial commit

---
+++ JAVA/ifesle/src/EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a number : ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println("even number ");
+        }
+        else {
+            System.out.println("odd number");
+        }
+    }
+}
---
+++ JAVA/ifesle/src/gradeCalculator.java
@@ -0,0 +1,15 @@
+public class gradeCalculator {
+
+
+}
+
+
+//Grade Calculator
+//
+//Question:
+//Marks ke according grade print karo:
+//
+//90+ → A
+//75-89 → B
+//50-74 → C
+//Below 50 → Fail
