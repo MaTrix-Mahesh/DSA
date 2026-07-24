5b1b30297824167dc5fd5794b7a0cc154980516b
mahesh sahu <shamahesh615@gmail.com>
2026-07-24 21:52:54 +0530

    initial commit

--- JAVA/ifesle/src/gradeCalculator.java
+++ JAVA/ifesle/src/gradeCalculator.java
@@ -1,5 +1,25 @@
-public class gradeCalculator {
+import java.util.Scanner;
 
+public class gradeCalculator {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+x
+
+        System.out.println("enter your marks : ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90 && marks <=100){
+            System.out.println("A+");
+        } else if (marks >= 75 && marks <=89) {
+            System.out.println("B+");
+        } else if (marks >= 50 && marks <=74) {
+            System.out.println("C+");
+        }
+        else {
+            System.out.println("upgrade");
+        }
+    }
 
 }
 
