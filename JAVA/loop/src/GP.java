4febb7b216f7812cabb08172ee664b6d73184552
mahesh sahu <shamahesh615@gmail.com>
2026-07-26 11:22:23 +0530

    initial commit

--- JAVA/loop/src/GP.java
+++ JAVA/loop/src/GP.java
@@ -1,3 +1,15 @@
 public class GP {
-    
+    static void main(String[] args) {
+        int n = 10;
+
+
+        int a= 1;
+        int r = 2;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a+" ");
+            a *=r;
+        }
+    }
 }
+// gp = a*r
---
+++ JAVA/loop/src/intermediate.java
@@ -0,0 +1,11 @@
+public class intermediate {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1, j = n; i <= n; i++, j--) {
+            System.out.println(i);
+            System.out.println(j);
+        }
+    }
+}
