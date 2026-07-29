e269b73e2b1a5b6e23705a9ad17ab333520ce5f4
mahesh sahu <shamahesh615@gmail.com>
2026-07-29 22:12:09 +0530

    fresh commit

--- JAVA/loop/src/prime.java
+++ JAVA/loop/src/prime.java
@@ -1,4 +1,31 @@
-public class prime {
+import java.util.Scanner;
 
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your number: ");
+        int num = sc.nextInt();
+
+        boolean flag = true;
+
+        if (num <= 1) {
+            System.out.println("Neither Prime nor Composite");
+        } else {
+            for (int i = 2; i <= num - 1; i++) {
+                if (num % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                System.out.println("Prime Number");
+            } else {
+                System.out.println("Composite Number");
+            }
+        }
+
+        sc.close();
+    }
 }
-// prime-- 2 he factor ho
