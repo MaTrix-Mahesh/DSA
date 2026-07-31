29b990b53ff318e3e55c22c548e24632985e02ce
mahesh sahu <shamahesh615@gmail.com>
2026-07-31 12:15:41 +0530

    initial commit

--- JAVA/loop/src/sumDigit.java
+++ JAVA/loop/src/sumDigit.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class sumDigit {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter your number : ");
-        int i = sc.nextInt();
 
-        int sum = 0 ;
-        while (i !=0){
-            int digit= i%10;
-            sum = sum+digit;
-            i = i/10;
+        System.out.print("enter a number you wnt to add : ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        while (n !=0){
+            int lastdigit = n%10;
+            sum = sum+lastdigit;
+            n = n/10;
         }
         System.out.print(sum);
     }
