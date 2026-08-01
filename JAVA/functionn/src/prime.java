d50ebcc7a1806cf99c54549e674e65f98fb18495
mahesh sahu <shamahesh615@gmail.com>
2026-08-01 22:46:25 +0530

    initial

--- JAVA/functionn/src/prime.java
+++ JAVA/functionn/src/prime.java
@@ -1,12 +1,12 @@
 public class prime{
-    public  void isPrime(int n) {
+    public boolean isPrime(int n) {
 
-        for (int i = 2; i <n-1 ; i++) {
-            if (n%i !=0){
-                return true;
+        for (int i = 2; i <= Math.sqrt(n) ; i++) {
+            if (n%i==0){
+                return false;
             }
         }
-        return false ;
+        return true  ;
     }
     public static void main(String[] args) {
         System.out.println(isPrime(5));
