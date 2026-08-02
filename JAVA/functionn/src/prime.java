3f706c747d3f950bf9abfd71ce828694ec926ba4
mahesh sahu <shamahesh615@gmail.com>
2026-08-02 10:10:59 +0530

    initial commit

--- JAVA/functionn/src/prime.java
+++ JAVA/functionn/src/prime.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class prime{
-    public boolean isPrime(int n) {
+    public static boolean isPrime(int n) {
 
         for (int i = 2; i <= Math.sqrt(n) ; i++) {
             if (n%i==0){
@@ -9,7 +11,11 @@ public class prime{
         return true  ;
     }
     public static void main(String[] args) {
-        System.out.println(isPrime(5));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your number : ");
+        int n = sc.nextInt();
+
+        System.out.print(isPrime(n));
     }
 }
 //Write a function isPrime(int n) that returns true if the number is prime.
