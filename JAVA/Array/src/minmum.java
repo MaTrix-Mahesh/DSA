0d482179babda3ce1c54032bebe49cae8f161534
mahesh sahu <shamahesh615@gmail.com>
2026-08-05 18:47:38 +0530

    initial commit

---
+++ JAVA/Array/src/minmum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class minmum {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("entera size of array : ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        //input
+        for (int i = 0; i <size ; i++) {
+            System.out.print("enter your element : ");
+            arr[i] = sc.nextInt();
+        }
+        // max
+        int max = arr[0];
+        for (int i = 1; i < size; i++) {
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.print(max +" ");
+    }
+}
