7f84e3ed2390b5cee69c65b46e58abeb9002b595
mahesh sahu <shamahesh615@gmail.com>
2026-08-12 22:36:35 +0530

    initial commit

---
+++ JAVA/loop/src/timecomplexity.java
@@ -0,0 +1,8 @@
+public class timecomplexity {
+    static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i <n ; i*=2) {
+            System.out.println(i);
+        }
+    }
+}
